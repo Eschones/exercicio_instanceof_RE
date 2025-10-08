d1a94a5eec7bae8e636903036e1c64eca0687b80
Raphael Esch <raphael.esch@hotmail.com>
2025-10-08 19:36:17 -0300

    Primeiro commit

---
+++ src/br/com/exemplo/TestarInstanceOf.java
@@ -0,0 +1,11 @@
+package br.com.exemplo;
+/*
+ * esta classe implementa o conceito de verificacao de instancias de classe
+ * atraves do metodo instanceof
+ */
+public class TestarInstanceOf {
+    
+    public static void main(String[] args) {
+
+    }
+}
